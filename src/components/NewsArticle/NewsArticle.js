75c8473c5854219b5b6e5639b966e550fe75bc0b
Cameron MacRae <cam.mac.cm@gmail.com>
2019-11-19 15:31:54 -0700

    Add grid styles

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -6,8 +6,9 @@ const NewsArticle = props => {
     return (
       <article className="article-wrap">
         <img src={story.img} className="article-img"/>
-        <h3>{story.headline}</h3>
-        <p>{story.description}</p>
+        <h3 className="article-head">{story.headline}</h3>
+        <p className="article-body">{story.description}</p>
+        <button type="button" className="article-link">View Article</button>
       </article>
     )
   });
