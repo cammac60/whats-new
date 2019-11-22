ea23cc3fde9aac685264acd25d569ed004ed5547
Cameron MacRae <cam.mac.cm@gmail.com>
2019-11-21 22:08:58 -0700

    Minor refactor

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,7 +6,7 @@ const SearchForm = props => {
     <header>
       <h1>Whats<span>New</span></h1>
       <div id="input-wrap">
-        <input onKeyPress={() => props.enterKeyListener(event)} onChange={props.resetError} id="search-input" type="text" placeholder="Search for a story..."></input>
+        <input onKeyPress={(event) => props.enterKeyListener(event)} onChange={props.resetError} id="search-input" type="text" placeholder="Search for a story..."></input>
         <label id="search-error" name="search-error">No results. Please try again.</label>
       </div>
       <button onClick={props.searchFn}  className="search-btn" type="button">Search</button>
